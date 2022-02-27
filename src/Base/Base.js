04ce01e97948513292e6f359964914df7cd94f95
SpongeBed <cmodz7969@gmail.com>
2022-02-27 17:00:17 +0300

    added embed manager

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -1,30 +1,7 @@
 const Client = require("../Client/Client")
 const eventsreq = require("../Client/Client")
-
-exports.Embed = void 0;
-class Embed {
-    constructor() {
-     
-    }
-    setAuhtor(authorget) {
-        this.auhtor = authorget
-        return this
-    }
-    setTitle(titleget) {
-        this.title = titleget
-        return this
-    }
-    setDescription(descriptionget) {
-        this.description = descriptionget
-        return this
-    }
-    setColor(colorcode) {
-        this.color = colorcode
-        return this
-    }
-}
-
+const embedmanager = require("../Managers/EmbedManager")
 
 exports.Client = Client.Client
 exports.events = eventsreq.events
-exports.Embed = Embed
+exports.Embed = embedmanager.Embed
---
+++ src/Managers/EmbedManager.js
@@ -0,0 +1,24 @@
+exports.Embed = void 0;
+class Embed {
+    constructor() {
+     
+    }
+    setAuhtor(authorget) {
+        this.auhtor = authorget
+        return this
+    }
+    setTitle(titleget) {
+        this.title = titleget
+        return this
+    }
+    setDescription(descriptionget) {
+        this.description = descriptionget
+        return this
+    }
+    setColor(colorcode) {
+        this.color = colorcode
+        return this
+    }
+}
+
+exports.Embed = Embed

2aed39e9b0f6cede0cd35130300de7e2ef6208dc
SpongeBed <56435044+SpongeBed81@users.noreply.github.com>
2022-02-27 19:13:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 async function sa() {
     const plex = require("./src/Base/Base")
 
-    const client = new plex.Client("ZORT")
+    const client = new plex.Client("zort")
     const clientEvents = plex.events
     client.start()
     clientEvents.on("ready", async() => {
         console.log("açıldı")
     })
     clientEvents.on("message", async(msg) => {
-        if(msg.content === "!ping") {
-            msg.guild.scheduleEvent({
-                name: "ping",
-                scheduled_start_time: Date.now() + 5000,
-                description: "test",
-                channel_id: "947512569676054548",
-                entity_type: "VOICE",
-                privacy_level: "GUILD_ONLY",
-            })
-        }
+        if(msg.author.bot) return
+        msg.reply("merhaba")
     })
 
 }

dec04eac1dbd09ee3b9dbce6ba79b54a9e6225db
SpongeBed <56435044+SpongeBed81@users.noreply.github.com>
2022-02-27 19:12:15 +0300

    Update GuildManager.js

--- src/Managers/GuildManager.js
+++ src/Managers/GuildManager.js
@@ -5,6 +5,8 @@ module.exports = {
     fetch: function(id) {
         return acs.req(`https://discord.com/api/v9/guilds/${id}`, "GET").then(async(dataget1) => {
             return new Promise((resolve, reject) => {
+
+                //BAN MEMBER
                 dataget1.banMember = function(obj) {
                     return new Promise((resolve, reject) => {
                         return acs.req(`https://discord.com/api/v9/guilds/${dataget1.id}/bans/${obj.id}`, "PUT", {
@@ -143,6 +145,90 @@ module.exports = {
                         })
                     })
                 }
+
+                //GUILD CHANNEL SYSTEM
+                dataget1.channels = {
+                    create: function(obj) {
+                        return new Promise((resolve, reject) => {
+                            return acs.req(`https://discord.com/api/v9/guilds/${dataget1.id}/channels`, "POST", {
+                                name: obj.name,
+                                type: obj.type,
+                                topic: obj.topic,
+                                position: obj.position,
+                                permission_overwrites: obj.permissionOverwrites,
+                                bitrate: obj.bitrate,
+                                user_limit: obj.userLimit,
+                                parent_id: obj.parentID,
+                                nsfw: obj.nsfw,
+                                rate_limit_per_user: obj.rateLimitPerUser,
+                                reason: obj.reason
+                            }).then(dataget => {
+                                resolve(dataget)
+                            })
+                        })
+                    }
+                }
+
+                //SET GUILD NAME 
+                dataget1.setName = function(name) {
+                    return new Promise((resolve, reject) => {
+                        return acs.req(`https://discord.com/api/v9/guilds/${dataget1.id}`, "PATCH", {
+                            name: name
+                        }).then(dataget => {
+                            resolve(dataget)
+                        })
+                    })
+                }
+                //SET GUILD REGION
+                dataget1.setRegion = function(region) {
+                    return new Promise((resolve, reject) => {
+                        return acs.req(`https://discord.com/api/v9/guilds/${dataget1.id}`, "PATCH", {
+                            region: region
+                        }).then(dataget => {
+                            resolve(dataget)
+                        })
+                    })
+                }
+
+                //LEAVE FROM GUILD
+                dataget1.leave = function() {
+                    return new Promise((resolve, reject) => {
+                        return acs.req(`https://discord.com/api/v9/users/@me/guilds/${dataget1.id}`, "DELETE").then(dataget => {
+                            resolve(dataget)
+                        })
+                    })
+                }
+                //SET GUILD ICON
+                dataget1.setIcon = function(icon) {
+                    return new Promise((resolve, reject) => {
+                        return acs.req(`https://discord.com/api/v9/guilds/${dataget1.id}`, "PATCH", {
+                            icon: icon
+                        }).then(dataget => {
+                            resolve(dataget)
+                        })
+                    })
+                }
+
+                dataget1.setAFKChannel = function(obj) {
+                    return new Promise((resolve, reject) => {
+                        return acs.req(`https://discord.com/api/v9/guilds/${dataget1.id}`, "PATCH", {
+                            afk_channel_id: obj.id
+                        }).then(dataget => {
+                            resolve(dataget)
+                        })
+                    })
+                }
+                dataget1.setAFKTimeout = function(obj) {
+                    return new Promise((resolve, reject) => {
+                        return acs.req(`https://discord.com/api/v9/guilds/${dataget1.id}`, "PATCH", {
+                            afk_timeout: obj.timeout
+                        }).then(dataget => {
+                            resolve(dataget)
+                        })
+                    })
+                }
+
+
                 resolve(dataget1)
             })
         })

eb745bb5f1f6cfdd385ee3598ef8bb49cc5eb9a7
kardespro <61694826+kardespro@users.noreply.github.com>
2022-02-28 21:33:13 +0400

    Update embed.js

--- examples/embed.js
+++ examples/embed.js
@@ -1,1 +1,22 @@
 // Embed
+
+async function sa() {
+    const plex = require("../src/Base/Base")
+
+    const client = new plex.Client("zort")
+    const clientEvents = plex.events
+    client.start()
+    clientEvents.on("ready", async() => {
+        console.log("açıldı")
+    })
+    clientEvents.on("message", async(msg) => {
+        if(msg.author.bot) return msg.reply("Hello Bot ! ")
+        let plexEmbed = plex.Embed()
+        .setTitle("Plex.JS")
+        .setColor("green")
+        .setDescription(`Plex.JS  Wonderfull ! ");
+        msg.reply(plexEmbed)
+    })
+
+}
+sa()
